fb31505844795698ad653b3e05a3788f140f5201
syzer3@gmail.com <syzer3@gmail.com>
2017-07-06 19:55:38 +0200

    first version of algorithm

---
+++ index.js
@@ -0,0 +1,38 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5,
+// we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// > 6%0
+// NaN
+// > 6%1
+// 0
+
+
+function calculate() {
+  let n = 7
+  let i = 1
+
+  while (i < n) {
+    if (n % i === 0) {
+      console.log('i')
+    } else {
+
+    }
+    i++
+  }
+
+
+  // 7/3 = ?
+  // 7/5 = ?
+  // if is => +1
+
+  return 3
+}
+
+
+// calculate() === 23
+
+
+//
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+
