fa36686d0eefd7a77686f8f697170dc36a0d01f5
syzer3@gmail.com <syzer3@gmail.com>
2017-07-06 20:38:35 +0200

    changed something

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5,
 // we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-// > 6%0
-// NaN
-// > 6%1
-// 0
 
-function calculate() {
+//
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+function calculate(max) {
   let i = 2
-  let max = 10
   let sum = 0
 
   while (i < max) {
@@ -27,7 +25,5 @@ let resultOf10 = calculate(10)
 console.log(resultOf10 === 23)
 
 
-//
-// Find the sum of all the multiples of 3 or 5 below 1000.
 
 
